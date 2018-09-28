c525c283081f83246d55d91373a3144661ca82e6
Fkeito <>
2018-09-28 15:02:01 +0900

    タイトル画面つける

---
+++ FlappyBird/Assets/Scripts/GameStartManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartManager : MonoBehaviour {
+
+    public Rigidbody player;
+    public ObstacleController[] obstacles;
+    public GameObject startCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            startCanvas.SetActive(false);
+            player.useGravity = true;
+            foreach(ObstacleController oc in obstacles){
+                oc.enabled = true;
+            }
+
+            Destroy(this);
+        }
+	}
+}
